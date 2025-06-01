b6ba79eab6dc49a38326c1868b3df3313d2799a4
Mihailghrgh <mihailghrgh@gmail.com>
2025-06-01 23:20:18 +0300

    Fix 3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,8 @@
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import { FlatCompat } from "@eslint/eslintrc";
-import { config as tseslintConfig } from "@typescript-eslint/eslint-plugin";
+import pkg from "@typescript-eslint/eslint-plugin";
+const { config: tseslintConfig } = pkg;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,11 +14,10 @@ const compat = new FlatCompat({
 export default [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 
-  // Use tseslint to apply strong TS rules
   ...tseslintConfig({
     rules: {
       "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": ["error"], 
+      "@typescript-eslint/no-unused-vars": "error",
     },
   }),
 ];
